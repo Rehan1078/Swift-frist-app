8619bf1e434293dcdacca50e9b41c2410e7d811a
Rehan <mrehan249249@gmail.com>
2026-03-15 16:33:40 +0500

    Init

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Muhammad Rehan Yousaf on 15/03/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
